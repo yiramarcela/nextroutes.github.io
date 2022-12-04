9da6985be06718877dde69363e29247a0b84c25e
MichaelRomero96 <michael.romero@meltstudio.co>
2022-12-04 09:59:34 -0500

    set next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
 }
 
 module.exports = nextConfig
